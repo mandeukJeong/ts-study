3875985e55375f04e87dbdd12eb4ca260a4deea5
mandeukJeong <wjdalstj137@gmail.com>
2024-03-25 22:15:32 +0900

    part1: typescript 기본 타입 정리

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var 이름 = 'kim';
-var john = [123, true];
-function 함수(x) {
-    return x * 2;
-}
+var 나이 = 50;
+// let 결혼했니 :boolean = true;
+// let 결혼했니 :null = null;
+var 결혼했니 = undefined;
+var 회원들 = ['kim', 'park'];
+var 회원들2 = { member1: 'kim', member2: 'park' };
+var 회원들3 = [1, 2, 3];
--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-type Name = string | number;
-type Member = [number, boolean];
+let 이름 :string = 'kim';
+let 나이 :number = 50;
+// let 결혼했니 :boolean = true;
+// let 결혼했니 :null = null;
+let 결혼했니 :undefined = undefined;
 
-let 이름 :Name = 'kim';
-let john :Member = [123, true];
-
-function 함수(x: number) :number {
-    return x * 2
-}
-
+let 회원들 :string[] = ['kim', 'park'];
+let 회원들2 :{ member1: string, member2: string } = { member1: 'kim', member2: 'park' }; 
+let 회원들3 :number[] = [1, 2, 3];
