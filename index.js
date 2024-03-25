f31c9889799a96238ec68ec3a75c69de9234077b
mandeukJeong <wjdalstj137@gmail.com>
2024-03-25 22:04:16 +0900

    part1: typescript 필수 문법

---
+++ index.js
@@ -0,0 +1,5 @@
+var 이름 = 'kim';
+var john = [123, true];
+function 함수(x) {
+    return x * 2;
+}
---
+++ index.ts
@@ -0,0 +1,10 @@
+type Name = string | number;
+type Member = [number, boolean];
+
+let 이름 :Name = 'kim';
+let john :Member = [123, true];
+
+function 함수(x: number) :number {
+    return x * 2
+}
+
