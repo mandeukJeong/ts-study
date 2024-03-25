9662518fb49acb06484500e6754deb9ead70caa2
mandeukJeong <wjdalstj137@gmail.com>
2024-03-25 22:19:47 +0900

    part1: typescript 기본 타입 정리 과제

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ var 결혼했니 = undefined;
 var 회원들 = ['kim', 'park'];
 var 회원들2 = { member1: 'kim', member2: 'park' };
 var 회원들3 = [1, 2, 3];
+var myName = '정민서';
+var myAge = 25;
+var myArea = '광명';
+var myFavorite = { song: "호무라", singer: "Lisa" };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true
+};
--- index.ts
+++ index.ts
@@ -7,3 +7,15 @@ let 결혼했니 :undefined = undefined;
 let 회원들 :string[] = ['kim', 'park'];
 let 회원들2 :{ member1: string, member2: string } = { member1: 'kim', member2: 'park' }; 
 let 회원들3 :number[] = [1, 2, 3];
+
+let myName :string = '정민서';
+let myAge :number = 25;
+let myArea :string = '광명';
+
+let myFavorite :{ song: string, singer: string } =  { song: "호무라", singer: "Lisa" };
+
+let project :{ member :string[], days :number, started :boolean } = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true
+};
