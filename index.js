14439c16b3289a55ddbcc85ce5a9ed0a7b68e505
mandeukJeong <wjdalstj137@gmail.com>
2024-03-26 23:59:14 +0900

    part1: 함수 타입 지정, void 타입 과제

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ function 함수(x) {
 function 함수1(x) {
     1 + 1;
 }
+function hello(name) {
+    if (name) {
+        return "안녕하세요" + name;
+    }
+    else {
+        return "이름이 없습니다";
+    }
+}
+console.log(hello("홍길동"));
+console.log(hello());
+function count(some) {
+    if (typeof (some) === "string") {
+        return some.length;
+    }
+    else {
+        var num = some.toString();
+        return num.length;
+    }
+}
+console.log(count(234));
+console.log(count('dfsdf'));
+function canMarry(money, isHome, score) {
+    var count = 0;
+    count += (money / 10000);
+    count = isHome ? count += 500 : count;
+    count = score === "상" ? count += 100 : count;
+    return count >= 600 ? "결혼가능" : null;
+}
--- index.ts
+++ index.ts
@@ -5,3 +5,35 @@ function 함수(x :number) :number {
 function 함수1(x? :number) :void {
     1 + 1;
 }
+
+function hello(name? :string) :string {
+    if (name) {
+        return "안녕하세요" + name;
+    } else {
+        return "이름이 없습니다";
+    }
+}
+
+console.log(hello("홍길동"));
+console.log(hello());
+
+function count(some :number | string) :number {
+    if (typeof(some) === "string") {
+        return some.length;
+    } else {
+        const num = some.toString();
+        return num.length;
+    }
+}
+
+console.log(count(234));
+console.log(count('dfsdf'));
+
+function canMarry(money :number, isHome :boolean, score :string) {
+    let count = 0;
+    count += (money / 10000);
+    count = isHome ? count += 500 : count;
+    count = score === "상" ? count += 100 : count;
+
+    return count >= 600 ? "결혼가능" : null;
+}
