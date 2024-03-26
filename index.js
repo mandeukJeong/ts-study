caeea469ba5f30dd4f6e689296207bbfe1a86c23
mandeukJeong <wjdalstj137@gmail.com>
2024-03-26 23:47:45 +0900

    part1: 함수 타입 지정, void 타입

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-var 회원 = 'kim';
-var 회원들 = [1, '2', 3];
-var 오브젝트 = { a: 123 };
-// let 이름 :any;
-// 이름 = 123;
-// 이름 = [];
-var 이름;
-// 이름 - 1;
-// 이름 = 123;
-// 이름 = {};
-// let 변수1 :string = 이름;
-var 나이;
-// 나이 + 1;
-var user = 'kim';
-var age = undefined;
-var married = false;
-var 철수 = [user, age, married];
-var 학교 = {
-    score: [100, 97, 84],
-    teacher: 'Phil',
-    friend: 'John'
-};
-학교.score[4] = false;
-학교.friend = ['Lee', 학교.teacher];
+function 함수(x) {
+    return x * 2;
+}
+function 함수1(x) {
+    1 + 1;
+}
--- index.ts
+++ index.ts
@@ -1,32 +1,7 @@
-let 회원 :(number | string) = 'kim';
-let 회원들 :(number | string)[] = [1, '2', 3];
-let 오브젝트 :{ a : (string | number) } = { a : 123 };
+function 함수(x :number) :number {
+    return x * 2;
+}
 
-// let 이름 :any;
-// 이름 = 123;
-// 이름 = [];
-
-let 이름 :unknown;
-// 이름 - 1;
-// 이름 = 123;
-// 이름 = {};
-
-// let 변수1 :string = 이름;
-
-let 나이 :string | number;
-// 나이 + 1;
-
-let user :string = 'kim';
-let age :undefined | number = undefined;
-let married :boolean = false;
-let 철수 :(string | undefined | number | boolean)[] = [user, age, married];
-
-let 학교 :{ score :(number | boolean)[], teacher :string, friend: (string | string[])} = {
-    score : [100, 97, 84],
-    teacher : 'Phil',
-    friend : 'John'
-};
-
-
-학교.score[4] = false;
-학교.friend = ['Lee' , 학교.teacher]
+function 함수1(x? :number) :void {
+    1 + 1;
+}
