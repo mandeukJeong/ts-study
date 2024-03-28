224c619ed7f9d777e51dc3de51873871d93a848b
mandeukJeong <wjdalstj137@gmail.com>
2024-03-28 22:55:07 +0900

    part1: 함수, method에 type alias 적용 과제

--- type_alias.js
+++ type_alias.js
@@ -3,7 +3,26 @@ var test = function (a) {
 };
 var 회원정보 = {
     name: 'kim',
+    age: 30,
     plusOne: function (a) {
         return a + 1;
+    },
+    changeName: function () {
+        console.log('안녕');
     }
 };
+회원정보.plusOne(1);
+회원정보.changeName();
+var cutZero = function (x) {
+    return x.replace(/^0+/, "");
+};
+function removeDash(x) {
+    return parseFloat(x.replace(/-/g, ""));
+}
+function callback(a, func1, func2) {
+    var result = func1(a);
+    var result2 = func2(result);
+    console.log(result2);
+}
+;
+callback('010-1111-2222', cutZero, removeDash);
--- type_alias.ts
+++ type_alias.ts
@@ -1,7 +1,9 @@
 type 함수타입 = (a :string) => number;
 type Member = {
     name : string,
-    plusOne: (a : number) => number;
+    age : number,
+    plusOne: (a : number) => number,
+    changeName: () => void
 }
 
 let test :함수타입 = function (a) {
@@ -10,7 +12,34 @@ let test :함수타입 = function (a) {
 
 let 회원정보 : Member = {
     name: 'kim',
+    age: 30,
     plusOne(a) {
         return a + 1;
+    },
+    changeName : () => {
+        console.log('안녕');
     }
 };
+
+회원정보.plusOne(1);
+회원정보.changeName();
+
+type CutType = (x : string) => string;
+
+let cutZero : CutType = function(x) {
+    return x.replace(/^0+/, "");
+};
+
+function removeDash(x : string) :number {
+    return parseFloat(x.replace(/-/g, ""));
+}
+
+type CutType2 = (x : string) => number
+
+function callback (a : string, func1 : CutType, func2 : CutType2) {
+    let result = func1(a);
+    let result2 = func2(result);
+    console.log(result2);
+};
+
+callback('010-1111-2222', cutZero, removeDash);
