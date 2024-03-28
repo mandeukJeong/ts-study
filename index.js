9421a668697c9ed835cb71354654003cea2ff8bf
mandeukJeong <wjdalstj137@gmail.com>
2024-03-28 18:33:58 +0900

    part1: Narrowing, Assertion

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-function 함수(x) {
-    return x * 2;
-}
-function 함수1(x) {
-    1 + 1;
-}
-function hello(name) {
-    if (name) {
-        return "안녕하세요" + name;
+function 내함수(x) {
+    var array = [];
+    if (typeof x === "number") {
+        array[0] = x;
     }
-    else {
-        return "이름이 없습니다";
-    }
+    // array[0] = x as number;
 }
-console.log(hello("홍길동"));
-console.log(hello());
-function count(some) {
-    if (typeof (some) === "string") {
-        return some.length;
-    }
-    else {
-        var num = some.toString();
-        return num.length;
-    }
-}
-console.log(count(234));
-console.log(count('dfsdf'));
-function canMarry(money, isHome, score) {
-    var count = 0;
-    count += (money / 10000);
-    count = isHome ? count += 500 : count;
-    count = score === "상" ? count += 100 : count;
-    return count >= 600 ? "결혼가능" : null;
-}
+내함수(123);
--- index.ts
+++ index.ts
@@ -1,39 +1,9 @@
-function 함수(x :number) :number {
-    return x * 2;
-}
-
-function 함수1(x? :number) :void {
-    1 + 1;
-}
-
-function hello(name? :string) :string {
-    if (name) {
-        return "안녕하세요" + name;
-    } else {
-        return "이름이 없습니다";
+function 내함수(x :number|string) {
+    let array :number[] = [];
+    if (typeof x === "number") {
+        array[0] = x;
     }
-}
+    // array[0] = x as number;
+}   
 
-console.log(hello("홍길동"));
-console.log(hello());
-
-function count(some :number | string) :number {
-    if (typeof(some) === "string") {
-        return some.length;
-    } else {
-        const num = some.toString();
-        return num.length;
-    }
-}
-
-console.log(count(234));
-console.log(count('dfsdf'));
-
-function canMarry(money :number, isHome :boolean, score :string) {
-    let count = 0;
-    count += (money / 10000);
-    count = isHome ? count += 500 : count;
-    count = score === "상" ? count += 100 : count;
-
-    return count >= 600 ? "결혼가능" : null;
-}
+내함수(123);
