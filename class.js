adc4d76905a394fb666b824fe86446d1f8383463
mandeukJeong <wjdalstj137@gmail.com>
2024-03-29 16:22:55 +0900

    part1: js - prototype

--- class.js
+++ class.js
@@ -3,10 +3,13 @@ function 기계(q, w) {
   this.w = w;
 }
 
+기계.prototype.name = 'kim';
+
 var nunu = new 기계('consume', 'snowball');
 var garen = new 기계('strike', 'courage');
 
 console.log(nunu, garen);
+console.log(nunu.name);
 
 class Hero {
   constructor(q, w) {
@@ -14,6 +17,12 @@ class Hero {
     this.w = w;
   }
 }
+
+var array = [4, 2, 1];
+var array1 = new Array(4, 2, 1);
+array1.sort();
+array.sort();
+console.log(array, array1);
 
 // var nunu = {
 //   q: 'consume',
