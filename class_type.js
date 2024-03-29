553c0e6b463a9c38b68ea571f082f566fc8896cc
mandeukJeong <wjdalstj137@gmail.com>
2024-03-29 16:38:48 +0900

    part1: ts class 타입 지정 과제

--- class_type.js
+++ class_type.js
@@ -11,3 +11,40 @@ var 사람1 = new Person1('kim');
 var 사람2 = new Person1('park');
 사람1.함수('안녕');
 console.log(사람1, 사람2);
+var Car = /** @class */ (function () {
+    function Car(model, price) {
+        this.model = model;
+        this.price = price;
+    }
+    Car.prototype.tax = function () {
+        return this.price / 10;
+    };
+    return Car;
+}());
+var sonata = new Car('소나타', 3000);
+console.log(sonata);
+console.log(sonata.tax());
+var Word = /** @class */ (function () {
+    function Word() {
+        var param = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            param[_i] = arguments[_i];
+        }
+        var 숫자들 = [];
+        var 문자들 = [];
+        param.forEach(function (i) {
+            if (typeof i === "string") {
+                문자들.push(i);
+            }
+            else {
+                숫자들.push(i);
+            }
+        });
+        this.num = 숫자들;
+        this.str = 문자들;
+    }
+    return Word;
+}());
+var obj = new Word('kim', 3, 5, 'park');
+console.log(obj.num);
+console.log(obj.str);
--- class_type.ts
+++ class_type.ts
@@ -14,3 +14,44 @@ let 사람2 = new Person1('park');
 
 사람1.함수('안녕');
 console.log(사람1, 사람2);
+
+class Car {
+    model : string;
+    price : number;
+    constructor(model : string, price : number) {
+        this.model = model;
+        this.price = price;
+    }
+    tax() :number {
+        return this.price / 10;
+    }
+}
+
+let sonata = new Car('소나타', 3000);
+console.log(sonata);
+console.log(sonata.tax());
+
+class Word {
+    num : number[];
+    str : string[];
+
+    constructor(...param : (number | string)[]) {
+        let 숫자들 : number[] = [];
+        let 문자들 : string[] = [];
+
+        param.forEach((i) => {
+            if (typeof i === "string") {
+                문자들.push(i);
+            } else {
+                숫자들.push(i);
+            }
+        });
+
+        this.num = 숫자들;
+        this.str = 문자들;
+    }
+}
+
+let obj = new Word('kim', 3, 5, 'park');
+console.log(obj.num);
+console.log(obj.str);
